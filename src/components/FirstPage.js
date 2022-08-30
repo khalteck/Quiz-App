87df337b0baf51518d1ff639d493f3d192c0351a
Khalteck <khalidoyeneye@gmail.com>
2022-08-29 22:52:46 -0600

    firstpage remove navbar

--- src/components/FirstPage.js
+++ src/components/FirstPage.js
@@ -9,7 +9,7 @@ export default function FirstPage(props) {
     }
     //${firstPageState ? "fixed top-0 left-0" : "hidden"}
     return (
-        <div className={`${firstPageState ? "fixed top-0 left-0" : "hidden"} w-full bg-fuchsia-50 h-[100vh] border-b border-[#293264] flex justify-center items-center`}>
+        <div className={`${firstPageState ? "fixed top-0 left-0" : "hidden"} w-full bg-fuchsia-50 h-[100vh] z-[999] border-b border-[#293264] flex justify-center items-center`}>
             <div className="text-center text-[20px] font-[500]">
                 <h1 className="text-[35px] font-[900] mb-4 tracking-normal">Quizzical</h1>
                 <p>Its quiz time! Test your knowledge. <br/> You can do this.</p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import idea from "../images/icons8-idea.svg"
 
 export default function Navbar() {
     return (
-        <nav className="w-full font-dyna bg-fuchsia-50 h-[60px] sm:h-[50px] fixed bottom-0 sm:top-0 left-0 border-t sm:border-b border-rose-300 shadow-md flex justify-center items-center">
+        <nav className="w-full z-[2] font-dyna bg-fuchsia-50 h-[60px] sm:h-[50px] fixed bottom-0 sm:top-0 left-0 border-t sm:border-b border-rose-300 shadow-md flex justify-center items-center">
             <div className="flex items-center">
                 <h1 className="text-[30px]">Quizzical</h1>
                 <img alt="" src={idea} className="w-[40px] h-[40px] ml-2 idea"/>
